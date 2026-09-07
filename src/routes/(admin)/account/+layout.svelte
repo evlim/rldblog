0ec3ac06de223ba167d3fcbd09c12222204892fb
Evan Unit Lim <62005952+evlim@users.noreply.github.com>
2026-09-07 16:05:40 +0200

    Initial commit

---
+++ src/routes/(admin)/account/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { invalidate } from "$app/navigation"
+  import { onMount } from "svelte"
+
+  let { data, children } = $props()
+
+  let supabase = $derived(data.supabase)
+  let session = $derived(data.session)
+
+  onMount(() => {
+    const { data } = supabase.auth.onAuthStateChange((event, _session) => {
+      if (_session?.expires_at !== session?.expires_at) {
+        invalidate("supabase:auth")
+      }
+    })
+
+    return () => data.subscription.unsubscribe()
+  })
+</script>
+
+{@render children?.()}
